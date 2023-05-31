11bec61c12823fb43b172c5b63b9b739f4b2e916
ambertua <amber.tuamoheloa@yahoo.com>
2023-05-30 22:19:37 -0700

    Combine Music Library Features

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import './App.css';
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Suspense } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { createResource as fetchData } from './helper'
+import Spinner from './components/Spinner';
+
 
 function App() {
   let [searchTerm, setSearchTerm] = useState('')
-  let [data, setData] = useState([])
-  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState(null)
+  let [message] = useState('Search for Music!')
 
   useEffect(() => {
     if (searchTerm) {
-      document.title=`${searchTerm} Music`
-      const fetchData = async () => {
-        const response = await fetch(`https://itunes.apple.com/search?term=${searchTerm}`)
-        const resData = await response.json()
-        if(resData.results.length > 0) {
-          setData(resData.results)
-        } else {
-          setMessage('Not Found')
-        }
-      }
-      fetchData()
-  }
+      setData(fetchData(searchTerm))
+    }
   }, [searchTerm])
+
+  const renderGallery = () => {
+    if(data){
+        return (
+            <Suspense fallback={<Spinner/>} >
+                <Gallery data={data} />
+            </Suspense>
+        )
+    }
+}
+
 
   const handleSearch = (e, term) => {
     e.preventDefault()
@@ -31,11 +35,11 @@ function App() {
 
   return (
     <div className="App">
-      <SearchBar handleSearch={handleSearch} />
-      {message}
-      <Gallery data={data} />
+        <SearchBar handleSearch={handleSearch} />
+        {message}
+        {renderGallery()}
     </div>
-  );
+);
 }
 
 export default App;
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,18 +1,20 @@
 import GalleryItem from './GalleryItem'
 
-function Gallery(props){
+export default function Gallery(props) {
 
-    const display = props.data.map((item, index) => {
+    const Gallery = (props) => {
+        const data = props.data.result.read()
+
+        const display = data.map((item, index) => {
+            return (
+                <GalleryItem item={item} key={index} />
+            )
+        })
+
         return (
-            <GalleryItem item={item} key={index} />
+            <div>
+                {display}
+            </div>
         )
-    })
-
-    return (
-        <div>
-            {display}
-        </div>
-    )
+    }
 }
-
-export default Gallery
